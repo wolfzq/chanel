67f08ea56b59d8ff4a0c2aaf729d004eec9c8b25
zhangqiang <wolfzq@126.com>
2019-10-30 23:02:19 +0800

    试验上传一个文件

---
+++ ddd/Details.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    详情
+  </div>
+</template>
+<script>
+export default {
+  // props:{
+  //   "lid":"sting"
+  // },
+  data(){
+    return{
+      data:[]
+    }
+  },
+  mounted(){
+    let  lid=this.$router.query.pic_id;
+    let  url="http://localhost:4000/picture"
+    this.axios.get(url,{params:lid}).then(res=>{
+      console.log(result)
+      
+    })
+  }
+
+}
+</script>
---
+++ ddd/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="(item,i) of list" :key="i">
+       <li>item.laotop_id</li>
+       <!-- <button @click="todetail(i)">详情</button> -->
+       <router-link :to="{path:'/picture',query:{pic_id:item.pid}}">详情页</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      list:[{"pid":2,"laptop_id":3,"sm":"img/product/sm/57b12a31N8f4f75a3.jpg","md":"img/product/md/57b12a31N8f4f75a3.jpg","lg":"img/product/lg/57b12a31N8f4f75a3.jpg"},{"pid":8,"laptop_id":3,"sm":"img/product/sm/57ad359dNd4a6f130.jpg","md":"img/product/md/57ad359dNd4a6f130.jpg","lg":"img/product/lg/57ad359dNd4a6f130.jpg"},{"pid":9,"laptop_id":3,"sm":"img/product/sm/57ad8846N64ac3c79.jpg","md":"img/product/md/57ad8846N64ac3c79.jpg","lg":"img/product/lg/57ad8846N64ac3c79.jpg"}]
+    }
+  },
+  methods:{
+    todetail(i){
+      this.$router.push({path:`/Details/${i}`})
+    }
+  }
+}
+</script>
